ab7e927170756c5eecf89e5b286c6e4a9a700b5f
Sagastein <ishimwe.sage.3@gmail.com>
2024-02-22 02:05:20 +0200

    add cookie parser to send and handle user cookies

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import express from "express";
 const app = express();
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db";
 import { UserRouter } from "./routes/user.route";
 const port = process.env.PORT || 8080;
 app.use(express.json());
+app.use(cookieParser());
 const main = express.Router();
 app.use("/api/v1", main);
 main.use("/user", UserRouter);
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "OK" });
+  res.status(200).json({ message: "Welcome to my server" });
 });
 connectDB().then(() => {
   app.listen(port, () => {
